3c134a5153f106ba6f942cfd2ee702d9f4d66693
Khuong Nguyen <knn1989@tamu.edu>
2015-10-06 00:53:20 -0500

    First page up and running

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # def hello
+  #   render text: "welcome to the museum of american G.I!"
+  # end
 end
