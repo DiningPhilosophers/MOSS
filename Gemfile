3455ca422a7ffc29b138fa29b3a56992360edcec
Jan <dufek@tamu.edu>
2015-10-09 14:25:35 -0500

    Login and logout first draft untested.

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem 'mysql2', '~> 0.3.18'
 gem 'bootstrap-datepicker-rails'
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   get 'visitors_statistics/show'
+  get 'session/new'
 
   resources :admins
   resources :countries
@@ -11,4 +13,8 @@ Rails.application.routes.draw do
   get 'homepage/index'
 
   root 'homepage#index'
+
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 end
