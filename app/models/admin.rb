b1b31794add131f84d0d2a782a83941a83912ab5
Jan <dufek@tamu.edu>
2015-10-10 21:11:31 -0500

    Admin data validation

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,10 +1,15 @@
 class Admin < ActiveRecord::Base
-  before_save { self.email = email.downcase }
-  validates :name, presence: true, length: {maximum: 50}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: 255},
-            format: {with: VALID_EMAIL_REGEX},
-            uniqueness: {case_sensitive: false}
+  #before_save { self.email = email.downcase }
+
+  # validates user name for presence and length
+  validates :user_name, presence: true, length: {maximum: 50}
+
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #validates :email, presence: true, length: {maximum: 255},
+  #          format: {with: VALID_EMAIL_REGEX},
+  #          uniqueness: {case_sensitive: false}
+
+  # handles password
   has_secure_password
   validates :password, length: {minimum: 6}
 end
