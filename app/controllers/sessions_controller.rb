b6464991001201af4f097da7b8aeba004df75d7e
Jan <dufek@tamu.edu>
2015-10-11 02:07:02 -0500

    Basic login/logout system completed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,17 +2,18 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-    user = Admin.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+    admin = Admin.find_by(user_name: params[:session][:user_name])
+    if admin && admin.authenticate(params[:session][:password])
+      log_in admin
+      params[:session][:remember_me] == '1' ? remember(admin) : forget(admin)
+      redirect_to admin
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid user name/password combination'
       render 'new'
     end
   end
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'visitors_statistics/show'
   get 'session/new'
 
-  resources :admins
+  resources :admins # TODO We should probably remove this and do this manualy. We do not want somebody to get admins' data by visiting /admins/2
   resources :countries
   resources :zipcodes
   resources :surveys
@@ -14,9 +14,11 @@ Rails.application.routes.draw do
 
   root 'homepage#index'
 
+  # Routes for administrator login system
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
 
+  # Routes for adding a new administrator
   get 'signup'  => 'admins#new'
 end
