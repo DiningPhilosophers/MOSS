b3265b0cb47226f4d65c29a41f6f369d2e476879
Jan <dufek@tamu.edu>
2015-10-10 19:18:09 -0500

    Signup system first stage

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,6 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+
+  get 'signup'  => 'admins#new'
 end
