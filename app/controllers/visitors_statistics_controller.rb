ceac70258df10a633125cb7b46739cf5a6a1b388
Khuong Nguyen <knn1989@tamu.edu>
2015-10-29 18:29:11 -0500

    done some work on visitors statistics

--- app/controllers/visitors_statistics_controller.rb
+++ app/controllers/visitors_statistics_controller.rb
@@ -12,6 +12,14 @@ class VisitorsStatisticsController < ApplicationController
       @end_date = DateTime.strptime(@end_date,'%m/%d/%Y').at_end_of_day
     end
     @total_visitors = Visitor.where(:created_at => @start_date..@end_date).size;
+
+    @df = Array.new
+    @df <<'Addison'<<'Allen'<<'Carrollton'<<'Celina'<<'Coppell'<<'Flower Mound'<<'Plano'<<'Rockwall'<<'Frisco'<<'Irving'<<'Garland'<<'Sachse'<<'Grand Prairie'<<'The Colony'<<'Lewisville'<<'Lake Dallas'<<'Little Elm'<<'Mckinney'<<'Prosper'<<'Richardson'<<'Rowlett'<<'Wylie'<<'Bardwell'<<'Cedar Hill'<<'Crandall'<<'Desoto'<<'Duncanville'<<'Ennis'<<'Ferris'<<'Forney'<<'Fate'<<'Lancaster'<<'Caddo Mills'<<'Hutchins'<<'Kaufman'<<'Mabank'<<'Mesquite'<<'Palmer'<<'Red Oak'<<'Rosser'<<'Scurry'<<'Seagoville'<<'Terrell'<<'Josephine'<<'Waxahachie'<<'Lavon'<<'Wilmer'<<'Nevada'<<'Sunnyvale'<<'Royse City'<<'Dallas'<<'Greenville'<<'Princeton'<<'Anna'<<'Ben Franklin'<<'Campbell'<<'Celeste'<<'Blue Ridge'<<'Commerce'<<'Cooper'<<'Enloe'<<'Farmersville'<<'Klondike'<<'Lake Creek'<<'Lone Oak'<<'Melissa'<<'Pecan Gap'<<'Quinlan'<<'Wolfe City'<<'Arlington'<<'Aledo'<<'Alvarado'<<'Azle'<<'Bedford'<<'Boyd'<<'Burleson'<<'Cleburne'<<'Colleyville'<<'Crowley'<<'Euless'<<'Forreston'<<'Godley'<<'Grandview'<<'Grapevine'<<'Haslet'<<'Hurst'<<'Joshua'<<'Keene'<<'Kennedale'<<'Lillian'<<'Mansfield'<<'Maypearl'<<'Midlothian'<<'Millsap'<<'Newark'<<'Paradise'<<'Rhome'<<'Springtown'<<'Venus'<<'Weatherford'<<'Southlake'<<'Rio Vista'<<'Fort Worth'<<'Haltom City'<<'Naval Air Station/ Jrb'<<'North Richland Hills'<<'Denton'<<'Alvord'<<'Argyle'<<'Aubrey'<<'Decatur'<<'Keller'<<'Justin'<<'Krum'<<'Pilot Point'<<'Ponder'<<'Roanoke'<<'Sanger'<<'Bridgeport'<<'Chico'<<'Poolville'<<'Whitt'<<'Avalon'<<'Italy'<<'Milford'
+    @df_zipcodes = Zipcode.where(:city => @df)
+    @n_df_visitors = Visitor.where(:created_at => @start_date..@end_date).where(:zip_code => @df_zipcodes).size;
+
+    @austin_zipcodes = Zipcode.where(:city => 'Austin')
+    @n_austin_visitors = Visitor.where(:created_at => @start_date..@end_date).where(:zip_code => @austin_zipcodes).size;
     render :layout => 'admin'
   end
 end
