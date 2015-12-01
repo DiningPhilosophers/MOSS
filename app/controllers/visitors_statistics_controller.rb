ddabef9e32563ad060d3f0472c9b5f757100282e
Khuong Nguyen <knn1989@tamu.edu>
2015-12-01 08:14:56 -0600

    fix too large ajax get request for chartkick

--- app/controllers/visitors_statistics_controller.rb
+++ app/controllers/visitors_statistics_controller.rb
@@ -150,12 +150,17 @@ class VisitorsStatisticsController < ApplicationController
   def visitors_in_date
     @dates_result = []
     @dates = params[:dates]
-    @dates.each do |date|
-      @s = DateTime.strptime(date,'%m/%d/%Y').at_beginning_of_day
-      @e = DateTime.strptime(date,'%m/%d/%Y').at_end_of_day
-      # @dates_result.push(d:date,v:Visitor.where(:created_at => @s..@e).size)
-      @dates_result.push(d:date,v:Group.where(:created_at => @s..@e).pluck(:group_size).sum())
-    end
+
+    # if @dates.size > 31
+    #
+    # else
+      @dates.each do |date|
+        @s = DateTime.strptime(date,'%m/%d/%Y').at_beginning_of_day
+        @e = DateTime.strptime(date,'%m/%d/%Y').at_end_of_day
+        # @dates_result.push(d:date,v:Visitor.where(:created_at => @s..@e).size)
+        @dates_result.push(d:date,v:Group.where(:created_at => @s..@e).pluck(:group_size).sum())
+      end
+    # end
 
     # render :json => @dates
     render :json => @dates_result
