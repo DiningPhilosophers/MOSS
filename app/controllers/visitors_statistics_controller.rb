62ffaf467047f1268dd25afbf380dabde6c36276
odair10 <oda.fernandes10@gmail.com>
2015-12-03 16:53:14 -0600

    Main Page font changed and date range updated to 365 days

--- app/controllers/visitors_statistics_controller.rb
+++ app/controllers/visitors_statistics_controller.rb
@@ -7,7 +7,7 @@ class VisitorsStatisticsController < ApplicationController
 
     if(@start_date.blank? || @end_date.blank?)
       @end_date = DateTime.now.at_end_of_day
-      @start_date = (DateTime.now - 15.days)
+      @start_date = (DateTime.now - 365.days)
       @start_date = @start_date.at_end_of_day
     else
       @start_date = DateTime.strptime(@start_date,'%m/%d/%Y').at_beginning_of_day
